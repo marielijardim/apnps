7ac6829ae72f930a274a8919a2d4d1ea640598c5
Marieli <marielijardim190@gmail.com>
2021-03-30 19:35:39 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/', function(req,res){
+    res.send("Ola mari!")
+})
+
+app.listen(3000,function(){
+    console.log("Conexão inicializada na porta 3000")
+})
