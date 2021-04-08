63b09473c9371c4fb7ca0298cb4e2f28e7fbc6ed
Marieli <marielijardim190@gmail.com>
2021-04-08 19:24:43 -0300

    usando ejs nas páginas

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function(req,res){
-    res.send("Ola mari!")
+    res.render('index.ejs' ,{} )
+})
+
+app.get('/usuarios' , function(req,res){
+        res.render('usuarios.ejs' , { usuarios:[
+            {nome:'Mariéli', email:'marielijardim190@gmail.com'},
+            {nome:'Maria', email:'maria@hotmail.com'},
+            {nome:'Tereza', email:'tereza@hotmail.com'},
+            {nome:'Ana', email:'ana@hotmail.com'}
+        ] })
 })
 
 app.listen(3000,function(){
